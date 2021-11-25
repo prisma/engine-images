8d71d18591d80d167298639bfdf1314f4cf52697
Dominic Petrick <dompetrick@gmail.com>
2021-11-25 12:53:54 +0100

    Correct image

--- cockroach/prisma_init.sql
+++ cockroach/prisma_init.sql
@@ -1,6 +1,8 @@
 CREATE USER prisma;
 GRANT admin TO prisma;
+
 ALTER USER prisma SET null_ordered_last = true;
+
 SET CLUSTER SETTING sql.defaults.default_int_size = 4;
 SET CLUSTER SETTING sql.defaults.serial_normalization = 'sql_sequence';
 SET CLUSTER SETTING schemachanger.backfiller.buffer_increment = '128 KiB';
@@ -11,5 +13,6 @@ SET CLUSTER SETTING jobs.retention_time = '15s';
 SET CLUSTER SETTING jobs.registry.interval.cancel = '180s';
 SET CLUSTER SETTING jobs.registry.interval.gc = '30s';
 SET CLUSTER SETTING sql.stats.automatic_collection.enabled = false;
+
 ALTER RANGE default CONFIGURE ZONE USING gc.ttlseconds = '5';
 ALTER DATABASE system CONFIGURE ZONE USING gc.ttlseconds = '5';
