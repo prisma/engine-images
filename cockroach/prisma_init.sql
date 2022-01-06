123e1aac3416a350d64b8c0714e0b2cba7089900
Oliver Tan <otan@cockroachlabs.com>
2022-01-07 03:15:44 +1100

    cockroach: add another CI speedup to image (#35)

--- cockroach/prisma_init.sql
+++ cockroach/prisma_init.sql
@@ -11,6 +11,7 @@ SET CLUSTER SETTING jobs.retention_time = '15s';
 SET CLUSTER SETTING jobs.registry.interval.cancel = '180s';
 SET CLUSTER SETTING jobs.registry.interval.gc = '30s';
 SET CLUSTER SETTING sql.stats.automatic_collection.enabled = false;
+SET CLUSTER SETTING kv.range_split.by_load_merge_delay = '5s';
 
 ALTER RANGE default CONFIGURE ZONE USING gc.ttlseconds = '5';
 ALTER DATABASE system CONFIGURE ZONE USING gc.ttlseconds = '5';
