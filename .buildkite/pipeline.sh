15c89e53059a483363c6969526e1c509145de84c
Dominic Petrick <dompetrick@gmail.com>
2022-04-08 17:56:29 +0200

    Adjust build script

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -1,39 +1,72 @@
 #!/usr/bin/bash
 
-echo "steps:
-  - label: \":debian: Debian build image\"
-    command: cd debian && make build && make push
-    branches: main
+if [[ "$BUILDKITE_BRANCH" == "main" ]]; then
+    # Build & push images
+    echo "steps:
+    - label: \":debian: Debian build image\"
+      command: cd debian && make build && make push
+      branches: main
 
-  - label: \":rhel: RHEL build image\"
-    command: cd rhel && make build && make push
-    branches: main
+    - label: \":rhel: RHEL build image\"
+      command: cd rhel && make build && make push
+      branches: main
 
-  - label: \":linux: Musl build image\"
-    command: cd musl && make build && make push
-    branches: main
+    - label: \":linux: Musl build image\"
+      command: cd musl && make build && make push
+      branches: main
 
-  - label: \":rust::darwin: Cross compilation images\"
-    command: cd cross && make build && make push
-    branches: main
+    - label: \":rust::darwin: Cross compilation images\"
+      command: cd cross && make build && make push
+      branches: main
 
-  - label: \":rust: Test image\"
-    command: cd test && make build && make push
-    branches: main
+    - label: \":rust: Test image\"
+      command: cd test && make build && make push
+      branches: main
 
-  - label: \":rust: Release image\"
-    command: cd release && make build && make push
-    branches: main
+    - label: \":rust: Release image\"
+      command: cd release && make build && make push
+      branches: main
 
-  - label: \":rust: SQL Server images\"
-    command: cd sql_server && make build && make push
-    branches: main
+    - label: \":rust: SQL Server images\"
+      command: cd sql_server && make build && make push
+      branches: main
 
-  - label: \":mongodb: Mongo single-replica image\"
-    command: cd mongo && make build && make push
-    branches: main
+    - label: \":mongodb: Mongo single-replica image\"
+      command: cd mongo && make build && make push
+      branches: main
 
-  - label: \":ant: CockroachDB custom image\"
-    command: cd cockroach && make build && make push
-    branches: main
-" | buildkite-agent pipeline upload
+    - label: \":ant: CockroachDB custom image\"
+      command: cd cockroach && make build && make push
+      branches: main
+  " | buildkite-agent pipeline upload
+else
+    # Only build images
+    echo "steps:
+    - label: \":debian: Debian build image\"
+      command: cd debian && make build
+
+    - label: \":rhel: RHEL build image\"
+      command: cd rhel && make build
+
+    - label: \":linux: Musl build image\"
+      command: cd musl && make build
+
+    - label: \":rust::darwin: Cross compilation images\"
+      command: cd cross && make build
+
+    - label: \":rust: Test image\"
+      command: cd test && make build
+
+    - label: \":rust: Release image\"
+      command: cd release && make build
+
+    - label: \":rust: SQL Server images\"
+      command: cd sql_server && make build
+
+    - label: \":mongodb: Mongo single-replica image\"
+      command: cd mongo && make build
+
+    - label: \":ant: CockroachDB custom image\"
+      command: cd cockroach && make build
+  " | buildkite-agent pipeline upload
+fi
